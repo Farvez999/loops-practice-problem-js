12941d52138ab641941bc33fbcc9086fe9095f3e
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-02 21:05:13 +0600

    array-loop

---
+++ arrary-loop.js
@@ -0,0 +1,11 @@
+var numbers = [34, 45, 45, 87, 87, 78, 80, 399, 56];
+for (i = 0; i <= numbers.length; i++) {
+    var number = numbers[i];
+    console.log(number);
+}
+
+var items = ['Farvz', 'Mitu', 'Momo', 'Eshita', 'tasnu']
+for (i = 0; i <= items.length; i++) {
+    var item = items[i];
+    console.log(item);
+}
