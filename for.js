b5f131f95286f3ded3b31813f48fd6b3a3230d1a
Farvez Abdullah <engrfrzabdullah999@gmail.com>
2022-08-02 20:37:10 +0600

    for loop

---
+++ for.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 7; i++) {
+    console.log(i);
+}
+
+// even number 
+for (var i = 0; i <= 10; i += 2) {
+    console.log(i);
+}
+
+//odd number 
+for (var i = 1; i <= 10; i += 2) {
+    console.log(i);
+}
